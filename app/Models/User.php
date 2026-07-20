d1ec3755a20449e3a714d66824abc3ffacbc66cf
4NDR3S <ac20102003@gmail.com>
2026-07-20 12:51:13 -0500

    feat: implementar la interfaz de usuario de Filament en el modelo User para el acceso al panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,16 +7,27 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+    /**
+     * Filament 5 exige esta capa de seguridad adicional: si no devuelve true,
+     * el panel responde 403 Forbidden al iniciar sesión aunque las credenciales sean correctas.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * Get the attributes that should be cast.
